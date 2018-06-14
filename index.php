7d530fd5c8e600c2d1fce67126570d0ab63d29df
dalikimail <39304103+dalikimail@users.noreply.github.com>
2018-06-14 14:51:08 +0300

    Errors set to 0

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  *  @file index.php
  *  @brief Точка входа, ядро сайта грузится через core.php, запускается Router для перехвата GET запросов
  */
-ini_set('display_errors', 1);
+ini_set('display_errors', 0);
 require_once('classes/core.php');
 
 $Router = new Router( $_GET );
